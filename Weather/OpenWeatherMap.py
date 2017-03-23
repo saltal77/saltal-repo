3fd9c529bd6041b4db74f08ea8b8dbe57cebca3b
saltal77 <saltal@yandex.ru>
2017-03-23 15:00:33 +0300

    Open weather service

---
+++ Weather/OpenWeatherMap.py
@@ -0,0 +1,127 @@
+# coding: utf-8
+__author__ = "saltal"
+
+import gzip
+import json
+import codecs
+import re
+import os
+import patoolib
+import urllib.request
+import sqlite3
+from  datetime import date
+import sys
+
+print("Любая погода у вас дома с портала Openweathermap.org!")
+print("Загружаюсь, один момент pls...")
+
+# Чтение APP ID из файла
+with open('app.id', 'r', encoding='UTF-8') as ap:
+    app_id = ap.read()
+
+# Загрузка списка городов city.list.json.gz
+#destination = 'city.list.json.gz'
+#url_s = 'http://bulk.openweathermap.org/sample/city.list.json.gz'
+#urllib.request.urlretrieve(url_s, destination)
+
+# Распаковка списка городов в JSON формате
+#patoolib.extract_archive("city.list.json.gz", outdir = os.getcwd())
+
+# Чтение файла и обработка JSON списка
+with open('city.list.json', 'r', encoding='UTF-8') as f:
+    for line in f:
+        data = f.readlines()
+
+all_towns = dict()
+for towns in data:
+    town_dict = json.loads(towns)
+    all_towns[town_dict['_id']] = dict(name=town_dict['name'],\
+        country=town_dict['country'], id = town_dict['_id'])
+
+# Поиск города по начальным буквам
+strn = input("Введите первые буквы города: ")
+pattern = strn + '[a-z]+'
+
+lst_town = []
+for town in all_towns.values():
+    if (re.findall(pattern, town['name'])):
+        lst_town.append(re.findall(pattern, town['name']))
+
+for town in lst_town:
+    print(town)
+
+# Поиск города по полному имени
+id_town = []
+strn2 = input("Введите  город: ")
+for town in all_towns.values():
+    if town['name'] == strn2:
+        #print(town['id'], town['name'], town['country'])
+        id_town.append(town['id'])
+
+# Если данных о погоде нет - выход из прогрммы
+try:
+    id_town = str(id_town[0])
+except IndexError:
+    print("Нет данных о погоде, завершаю работу.")
+    sys.exit()
+
+# Загрузка и парсинг данных о погоде нужного города
+dt = str(date.today())
+url_w = 'http://api.openweathermap.org/data/2.5/weather?id=' + id_town +'&units=metric&appid=' + app_id
+w_town = urllib.request.urlopen(url_w)
+w_out = str(w_town.read(), 'utf-8')
+town_name = str(re.findall('(?:"name":)("\w+)', w_out))[3:-2]
+temp = str(re.findall('(?:"temp"):(.*?,)', w_out))[2:-3]
+id_whe = str(re.findall('(?:"id":)(\d{1,3},)', w_out))[2:-3]
+
+# Вывод на экран нужной погоды
+print('id {} Город {} Дата {} Температура {} Id Погоды {}'.format(id_town, town_name, dt,temp, id_whe))
+
+# Создание SQL базы
+sqlbase = 'wth.db'
+
+# Блок SQL Запросов
+sql_start = 'INSERT INTO weather (id_города, Город, Дата, Температура, id_погоды ) \
+VALUES(" ' + id_town + '","' + town_name + '","' + dt + '","' + temp + '","' + id_whe + '")'
+
+sql_crt = 'CREATE TABLE weather (id_города  INTEGER PRIMARY KEY, Город   VARCHAR(255), \
+Дата  DATE, Температура  INTEGER, id_погоды  INTEGER)'
+
+sql_upd = 'UPDATE weather SET Температура = "'+ temp +'" WHERE id_города = "'+ id_town +'"'
+
+sql_find = 'SELECT * FROM weather WHERE id_города = "'+ id_town +'"'
+
+# Работа с SQL базой
+conn = sqlite3.connect(sqlbase)
+cur = conn.cursor()
+try:
+    cur.execute(sql_crt)
+    conn.commit()
+    print("Создана  База Погоды ...")
+    cur.execute(sql_start)
+    conn.commit()
+    #cur.execute('SELECT * FROM weather')
+    #print(cur.fetchall())
+    conn.close()
+# Если БД уже создавалась
+except sqlite3.OperationalError:
+    cur.execute(sql_find)
+    data = cur.fetchone()
+    if data is None:
+        print("Добавляю Данные в Базу Погоды...")
+        cur.execute(sql_start)
+        conn.commit()
+        #cur.execute('SELECT * FROM weather')
+        #print(cur.fetchall())
+        conn.close()
+    else:
+        print("Обновляю Данные в Базе Погоды...")
+        cur.execute(sql_upd)
+        conn.commit()
+        #cur.execute('SELECT * FROM weather')
+        #print(cur.fetchall())
+        conn.close()
+
+input("Програма завершена, нажмите любую клавишу ...")
+
+
---
+++ Weather/export_openweather.py
@@ -0,0 +1,126 @@
+# coding: utf-8
+__author__ = "saltal"
+
+#формат запроса - export_openweather.py csv filename Town
+
+
+import sys
+import sqlite3
+import json
+import csv
+
+#Блок проверок аргументов
+try:
+    f_format = sys.argv[1]
+except IndexError:
+    print("Необходимо ввести формат файла csv или json...")
+    sys.exit()
+
+try:
+  f_name = sys.argv[2]
+except IndexError:
+    f_name = ''
+
+try:
+    town_name = sys.argv[3]
+except IndexError:
+    town_name = ''
+
+f_name_bkp = "backup"
+ending = [".json",".csv"]
+user_end = ["json","csv"]
+sqlbase = 'wth.db'
+
+#Проверка формата файла
+if  f_format != user_end[0] and f_format != user_end[1]:
+    print("Необходимо правильно ввести формат файла csv или json...")
+    sys.exit()
+
+#Блок Запросов
+sql_find = 'SELECT * FROM weather WHERE Город = "'+ town_name +'"'
+sql_all = 'SELECT * FROM weather'
+
+
+def json_bkp(req, f_name):
+    """
+     Функция сохраняет данные  SQLite в json формате
+    """
+    if f_name:
+        filename = f_name + ending[0]
+    else:
+        filename = f_name_bkp + ending[0]
+    conn = sqlite3.connect(sqlbase)
+    cur = conn.cursor()
+    cur.execute(req)
+    conn.commit()
+    json_string = json.dumps(cur.fetchall())
+    with open(filename, 'w', encoding='UTF-8', ) as f:
+        json.dump(json_string, f, ensure_ascii=False)
+    conn.close()
+    return
+
+def csv_bkp(req, f_name):
+    """
+    Функция сохраняет данные  SQLite в csv формате
+    """
+    if f_name:
+        filename = f_name + ending[1]
+    else:
+        filename = f_name_bkp + ending[1]
+    conn = sqlite3.connect(sqlbase)
+    cur = conn.cursor()
+    cur.execute(req)
+    conn.commit()
+    csvWriter = csv.writer(open(filename, "w"))
+    rows = cur.fetchall()
+    for row in rows:
+            csvWriter.writerow(row)
+    conn.close()
+
+#Блок сравнения поступивших данных и запросов к SQL
+if town_name:
+    #print("Город введен -  Поиск данных этого города...")
+    find = sql_find
+    conn = sqlite3.connect(sqlbase)
+    cur = conn.cursor()
+    cur.execute(find)
+    conn.commit()
+    if cur.fetchall():
+        print("Город найден в базе данных, выгружаю данные")
+        if f_format == 'json':
+            json_bkp(find, f_name)
+        else:
+            csv_bkp(find, f_name)
+    else:
+        print("Город не найден в базе данных, выгружаю всю базу")
+        if f_format == 'json':
+            json_bkp(sql_all, f_name)
+            conn.close()
+            sys.exit()
+        else:
+            csv_bkp(sql_all, f_name)
+            conn.close()
+            sys.exit()
+    conn.close()
+
+else:
+    print("Не введен город - выгрузка данных всей базы...")
+    find = sql_all
+    conn = sqlite3.connect(sqlbase)
+    cur = conn.cursor()
+    cur.execute(find)
+    conn.commit()
+    if cur.fetchall():
+        #print("Данные выгружаются...")
+        if f_format == 'json':
+            json_bkp(find, f_name)
+        else:
+            csv_bkp(find, f_name)
+    else:
+        print("Данные не найдены...")
+        conn.close()
+        sys.exit()
+    conn.close()
+
+
+
