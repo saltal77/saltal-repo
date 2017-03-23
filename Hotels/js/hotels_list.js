8928857f9af6116c7cfc77cda76989b1d9ccdb64
saltal77 <saltal@yandex.ru>
2017-03-23 15:33:04 +0300

    Форма поиска Отеля JSON native JS

---
+++ Hotels/js/hotels_list.js
@@ -0,0 +1,265 @@
+var hotels = [{
+    "title": "Rixwell Irina Hotel",
+    "stars": 3,
+    "rating": 4,
+    "country": "Латвия",
+    "price": 4992
+}, {
+    "title": "Holiday Inn Resort Kandooma",
+    "stars": 4,
+    "rating": 4,
+    "country": "Мальдивы",
+    "price": 6735
+}, {
+    "title": "Golden Ring Hotel (Отель Золотое кольцо)",
+    "stars": 5,
+    "rating": 4,
+    "country": "Россия",
+    "price": 2431
+}, {
+    "title": "Grand Rotana Resort & Spa",
+    "stars": 5,
+    "rating": 4,
+    "country": "Египет",
+    "price": 6902
+}, {
+    "title": "Maritim Pine Beach",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 15658
+}, {
+    "title": "Fantasia Hotel de Luxe",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 26768
+}, {
+    "title": "Avantgarde Hotel & Resort",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 8467
+}, {
+    "title": "Paloma Grida Resort & Spa",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 11508
+}, {
+    "title": "Аквамарин",
+    "stars": 5,
+    "rating": 4,
+    "country": "Россия",
+    "price": 1438
+}, {
+    "title": "PALOMA Renaissance Antalya Beach Resort & SPA",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 11957
+}, {
+    "title": "Курорт Царьград",
+    "stars": 4,
+    "rating": 4,
+    "country": "Россия",
+    "price": 2540
+}, {
+    "title": "Alva Donna Exclusive Hotel & Spa",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 27525
+}, {
+    "title": "PALOMA Oceana Resort",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 9275
+}, {
+    "title": "Репино Cronwell Park Отель",
+    "stars": 3,
+    "rating": 3,
+    "country": "Россия",
+    "price": 3110
+}, {
+    "title": "Golden 5 Emerald Resort",
+    "stars": 5,
+    "rating": 3,
+    "country": "Египет",
+    "price": 3215
+}, {
+    "title": "Golden 5 Paradise Resort",
+    "stars": 5,
+    "rating": 3,
+    "country": "Египет",
+    "price": 3250
+}, {
+    "title": "Golden 5 Diamond Resort",
+    "stars": 5,
+    "rating": 4,
+    "country": "Египет",
+    "price": 2796
+}, {
+    "title": "Golden 5 Almas Resort",
+    "stars": 5,
+    "rating": 3,
+    "country": "Египет",
+    "price": 2591
+}, {
+    "title": "Golden 5 The Club Hotel",
+    "stars": 4,
+    "rating": 4,
+    "country": "Египет",
+    "price": 4030
+}, {
+    "title": "Golden 5 Topaz Suites Hotel de luxe",
+    "stars": 4,
+    "rating": 4,
+    "country": "Египет",
+    "price": 3518
+}, {
+    "title": "Golden 5 Sapphire Suites Hotel de luxe",
+    "stars": 4,
+    "rating": 4,
+    "country": "Египет",
+    "price": 3124
+}, {
+    "title": "Атлас Парк Отель",
+    "stars": 4,
+    "rating": 4,
+    "country": "Россия",
+    "price": 1488
+}, {"title": "Bellis Deluxe", "stars": 5, "rating": 4, "country": "Турция", "price": 13715}, {
+    "title": "Sani Beach",
+    "stars": 5,
+    "rating": 4,
+    "country": "Греция",
+    "price": 5610
+}, {
+    "title": "Porto Sani",
+    "stars": 5,
+    "rating": 4,
+    "country": "Греция",
+    "price": 5127
+}, {
+    "title": "Limak Atlantis De Luxe Hotel & Resort",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 17526
+}, {"title": "Sani Club", "stars": 5, "rating": 4, "country": "Греция", "price": 4351}, {
+    "title": "Sunis Hotel SU",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 4210
+}, {
+    "title": "Sani Asterias",
+    "stars": 5,
+    "rating": 4,
+    "country": "Греция",
+    "price": 4225
+}, {
+    "title": "Country Resort (ex.HELIOPARK Country Resort)",
+    "stars": 4,
+    "rating": 4,
+    "country": "Россия",
+    "price": 1214
+}, {
+    "title": "SENTIDO Perissia managed by PALOMA Hotels",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 7401
+}, {
+    "title": "Orange County Resort",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 11725
+}, {
+    "title": "Kempinski Hotel The Dome",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 5442
+}, {
+    "title": "Limak Limra Hotel & Resort",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 15623
+}, {
+    "title": "L'Oceanica Beach Resort Hotel",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 19612
+}, {
+    "title": "Nirvana Lagoon Villas Suites & Spa",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 11067
+}, {
+    "title": "SPA-отель Фонград",
+    "stars": 3,
+    "rating": 4,
+    "country": "Россия",
+    "price": 1221
+}, {
+    "title": "Романов лес",
+    "stars": 4,
+    "rating": 4,
+    "country": "Россия",
+    "price": 1015
+}, {
+    "title": "Monte Casa Spa & Wellness",
+    "stars": 4,
+    "rating": 4,
+    "country": "Черногория",
+    "price": 2574
+}, {
+    "title": "Oceania Club & Spa (закрыт)",
+    "stars": 5,
+    "rating": 4,
+    "country": "Греция",
+    "price": 4229
+}, {
+    "title": "Centara Grand Island Resort & Spa",
+    "stars": 5,
+    "rating": 4,
+    "country": "Мальдивы",
+    "price": 7929
+}, {
+    "title": "Mardan Palace",
+    "stars": 5,
+    "rating": 4,
+    "country": "Турция",
+    "price": 6593
+}, {
+    "title": "Lillyland Beach Club",
+    "stars": 4,
+    "rating": 3,
+    "country": "Египет",
+    "price": 5313
+}, {
+    "title": "Гранд Отель Поляна",
+    "stars": 5,
+    "rating": 4,
+    "country": "Россия",
+    "price": 1556
+}, {
+    "title": "SENTIDO Sultan Beldibi managed by PALOMA Hotels (ех.Paloma Club Sultan Beldibi)",
+    "stars": 4,
+    "rating": 4,
+    "country": "Турция",
+    "price": 6887
+}, {"title": "Эммаус Волга Клаб", "stars": 4, "rating": 4, "country": "Россия", "price": 621}, {
+    "title": "Hotel 19",
+    "stars": 4,
+    "rating": 5,
+    "country": "Украина",
+    "price": 516
+}];
